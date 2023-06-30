bf0ae9562973545760062cb10bb30d2f524bab19
brunoneo <83879907+brunoneo@users.noreply.github.com>
2023-06-30 11:32:45 +0200

    Update script.js
    script final

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
+function appelAPI(city) {
+        let url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=ba3e0af2ffd376b524544686e22f6f81&units=metric&lang=fr';
+    
+        fetch(url)
+        .then((response) => response.json()
+        .then((data) => {
+            temp.textContent = `Temperature de \n${data.main.temp} °`;
+            tempRessent.textContent = `Temperature ressentie de \n${data.main.feels_like} °`;
+            descript.textContent = ` ${data.weather[0].description}`;
+            document.querySelector('#humidité').innerHTML =`Humidité ${data.main.humidity} %`;
+            ville.textContent = `METEO DE ${data.name}`;
+    
+            setTimeout(appelJson,36000000);
 
-    btn.onclick = () => {
-        const CLEAPI ='ba3e0af2ffd376b524544686e22f6f81';
-        const ville= champ.value
-        rafraich = function(){
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${CLEAPI}&units=metric&lang=fr`)
-        .then(reponse => reponse.json())
-        .then(data => {
-            document.getElementById("entrée").innerHTML="";
-            document.getElementById("bouton").innerHTML="";
-            output.textContent = "";
-            output2.textContent = "";
-            output.textContent = `Temperature de ${data.main.temp} °`;
-            output2.textContent = `Temperature ressentie de ${data.main.feels_like} °`;
-            output3.textContent = ` ${data.weather[0].description}`;
-            output4.textContent = `Humidité ${data.main.humidity} %`;
-            output5.textContent = `${data.name}`;
-            
-            setTimeout(rafraich,3600000);
-            console.table(data);
-        })};
+        }));}
+
+function appelJson(){
+        fetch('./conf.json')
+        .then((resp) =>resp.json()
+        .then((data) => appelAPI(data.ville))
+            );
+            }
         
-     rafraich();   
-    } 
+        appelJson(); 
  
  
